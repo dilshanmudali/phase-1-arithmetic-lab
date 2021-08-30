7f8a7fe85e18a0f57e464cb7ba9ebd640b164d44
Dilshan <dilshan.mudali@gmail.com>
2021-08-30 12:18:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1 
+let num3 = 16
+let num4 = 4
+const multiply = num1 * num2 
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = num3 / num4
+
+let max = Math.max(0,3,20)
